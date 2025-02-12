6c7f40d7338614a20174e0db97dc17cfef701fb7
au335296 <au335296@D54296>
2025-02-12 14:53:31 +0100

    Commit updates

--- TestImport_cpodutils.m
+++ TestImport_cpodutils.m
@@ -3,16 +3,20 @@
 
 % Import function is developed by J. MacAulay
 % Link to functions: https://github.com/macster110/fpodmat/tree/main
+
+%%  Call in functions
+addpath(genpath('C:\Users\au335296\OneDrive - Aarhus universitet\Documents\MATLAB\DTO_BioFlow\POD_Upload\cpodutils'));
+
 %% Get file paths of multiple files
 [files, path] = uigetfile('*.*', 'MultiSelect','on') ;
 SelectedFilePath = fullfile(path, files);
 
 %% Or use these to test
 %CFPOD .CP1 file
-CP1podfile = 'O:\Tech_Novana-Marsvin\CPODS\DATA\__DataCleanedForHELCOM\TextFileProcessed\FB__FermernBelt_Processed\FB1\FB1J 2015 04 16 POD1981 file01 PART 62d 1m.CP1';
+CP1podfile = 'O:\Tech_Novana-Marsvin\CPODS\DATA\__DataCleanedForHELCOM\TextFileProcessed\LB__LittleBelt_Processed\LB1\LB1E 2013 02 06 POD1988 file01 PART 54d 1m.CP1';
 
 %CPOD .CP3 file
-CP3podfile = 'O:\Tech_Novana-Marsvin\CPODS\DATA\__DataCleanedForHELCOM\TextFileProcessed\FB__FermernBelt_Processed\FB1\FB1K 2015 09 30 POD1776 file01 PART 41d 52m.CP3';
+CP3podfile = 'O:\Tech_Novana-Marsvin\CPODS\DATA\__DataCleanedForHELCOM\TextFileProcessed\LB__LittleBelt_Processed\LB1\LB1E 2013 02 06 POD1988 file01 PART 54d 1m.CP3';
 
 %FPOD FP1 file
 FP1podfile = 'O:\Tech_Novana-Marsvin\FPOD\DATA\CPODvFPOD\processed_FPOD\MDF1A 2020 10 27 FPOD_6311 series1 file0.FP1';
@@ -24,7 +28,7 @@ podfilepath = {CP1podfile, CP3podfile, FP1podfile, FP3podfile};
 
 %% Load pod data
 
-i=2;
+i=1;
 %import the data
 [podData] = importpoddata(podfilepath{i}, 'detstart', 0, 'maxdet', 10000); % specifying to import the first N clicks
 disp(['Imported ' num2str(length(podData)) ' clicks']);
@@ -40,4 +44,5 @@ c = frequency/1000;
 scatter(date, ampdB, 10, frequency, 'filled'); 
 ylabel('Amplitude (dB)'); 
 set(gca, 'FontSize', 14)
+
 
