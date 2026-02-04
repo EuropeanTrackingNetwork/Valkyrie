d12d9b82740c637ea09a2c726f19319e60a71190
ellenrjacobs <ellenjacobs17@gmail.com>
2026-02-04 13:55:07 +0100

    Only import first 38 cols, to delete excel macro cols

--- helpers/loadMetadataFile.m
+++ helpers/loadMetadataFile.m
@@ -1,6 +1,6 @@
 
 % Function to load a metadata file based on the selected file path.
-% Reads everything as text; datetime parsing is deferred to validateDatetime.
+% Reads everything as text; datetime parsing is deferred to createDateTime.
 
 
 function tbl = loadMetadataFile(fullpath)
@@ -9,9 +9,11 @@ function tbl = loadMetadataFile(fullpath)
         % Detect import options from file
         opts = detectImportOptions(fullpath, 'TextType', 'string');
 
-        % Force ALL columns to be read as text (string) to avoid auto datetime creation
-        % You can restrict this to specific columns if you prefer.
-        opts = setvartype(opts, opts.VariableNames, 'string');
+        % Import only the first 38 cols, deleting the extra excel macro cols
+        opts.SelectedVariableNames = opts.VariableNames(1:38);
+
+        % Force all columns to be read as text (string) to avoid auto datetime creation
+        opts = setvartype(opts, opts.SelectedVariableNames, 'string');
 
         % Read table with these options
         tbl = readtable(fullpath, opts);
