29d536271af7ffd898169c19232290135ed6b7ce
MiaKronborg <mialkronborg@gmail.com>
2026-02-27 14:23:46 +0100

    Remove app installer. Will be included in release instead.

--- README.md
+++ README.md
@@ -1,46 +1,90 @@
 
 
 # VALKYRIE
-
 Valkyrie is a tool developed within the DTO BioFlow project (https://dto-bioflow.eu/) to harmonize metadata and porpoise detection data from C‑PODs and F‑PODs. It takes raw detection files (CP1/CP3 or FP1/FP3 pairs) together with standardized deployment metadata and produces ETN‑ready output files that can be uploaded directly to the ETN Underwater Acoustics database.
 Valkyrie validates metadata, matches POD detection files to deployments, extracts detections from raw files, and formats all outputs according to ETN requirements.
 
 ## Installation
-VALKYRIE can be installed from
+VALKYRIE can be installed for windows platforms. 
+Installation and use of VALKYRIE does not require MATLAB license.
 
+1. Download the installer here [link to installer once we release].
+2. Run the installer by double-clicking. It can take a moment to open the installer as it will automatically install MATLAB Runtime. When the installation window appear follow the promtpts.
 
+## Usage
+After installation, VALKYRIE is ready to use. However, to be able to upload output files into the ETN database, you need to first register on the ETN database website [insert link].
 
+The steps for using VALKYRIE are:
+1. Register as a user on ETN.
+2. Prepare input files (metadata and raw C-POD or F-POD files).
+3. Upload raw POD files.
+4. Upload metadata file.
+5. Evaluate files matched between both POD files and metadata
+6. Process matched files.
+7. Evaluate output files.
+8. Upload output files to ETN.
 
-## Overview
-Valkyrie provides an automated workflow for preparing POD datasets for ingestion into ETN. After registering as an ETN user and obtaining a project number, users can upload one metadata CSV file and the raw POD detection files for their deployments. Valkyrie checks that the metadata file follows the required format, verifies that all POD detection files are complete and correctly paired, and links each file to the appropriate deployment.
+Note that only files for a single ETN project can be processed in VALKYRIE at a time. 
+Each step is explained in detail below.
 
-Once processing begins, Valkyrie extracts detections from each CP1/CP3 or FP1/FP3 file pair and converts both the detections and the metadata into harmonized ETN‑compatible formats. The application outputs three files: deployment metadata, receiver metadata, and a combined detections dataset containing all processed deployments.
+### 1. Register as a user on ETN
+This step is not required to run VALKYRIE. However, to upload files into the ETN database a user must first register in the ETN system.
+To do that visit [link].
 
-## Metadata Requirements
-Before using Valkyrie, prepare a single metadata CSV file containing one row per deployment. The metadata must follow the Valkyrie metadata template, which includes separating timestamps into year, month, day, and time columns. A template is available in this repository to help you prepare the file (\SampleDeployment\VALKYRIE Sample Metadata - Blank.xlsm). Once completed, export the file to CSV format before uploading it to Valkyrie.
+### 2. Prepare input files
+Input files for VALKYRIE include CP1 and CP3 files, FP1 and FP3 files, and corresponding metadata for each deployment where data is uploaded.
 
-## Uploading files
-Valkyrie accepts metadata and POD detection files for one ETN project at a time, but you may upload multiple deployments in a single session. Upload your raw POD detection files first. You can either select individual files or a folder; Valkyrie will automatically search through subfolders and detect valid file pairs.
+#### POD files:
+Accepted formats: 
+- .CP1 and .CP3
+- .FP1 and .FP3
 
+VALKYRIE accepts the files generated when data is offloaded from either C-PODs or F-PODs.
+To correctly extract the data, files must be provided in pairs, meaning that for C-POD data it is CP1 and CP3 and for F-POD data it is FP1 and FP3. 
+
+#### Metadata file
+Accepted format:
+- .CSV
+
+Metadata includes information on the deployments. Download a template for metadata here [link].
+The template has built in macros, allowing the user to fill in the date and time for a deployment, which will then automatically be filled in for the Year, Month, Date, Time columns.
+
+### 3. Upload raw POD files
+Once you have located the POD files you want to extract, click the upload button. It is possible to select either single files (OBS: make sure to select file pairs, CP1/CP3 or FP1/FP3) or a folder. If a folder is selected all files of the correct format in the folder and its subfolders are selected.
+
+At this step VALKYRIE checks that files appear in pairs and that there are no duplicate files.
+
+The selected files will be displayed in the window pane for viewing.
+
+### 4. Upload metadata files
 Once POD files are uploaded and validated, you can upload the metadata file. Valkyrie will validate all fields and report any issues.
 
-Each deployment must include both files in the pair: CP1 and CP3 for C‑PODs, or FP1 and FP3 for F‑PODs. Valkyrie will match these files to the corresponding deployment based on the metadata.
+VALKYRIE can only process files for a single ETN project at a time. If more than one project appear in the metadata, the user will be prompted to select which one they want to process. 
 
-## Processing and output
-When all files are uploaded, Valkyrie processes the data automatically. It extracts detections and converts everything into ETN‑standard formats.
+### 5. Evaluate file match
+When metadata has been validated the user can click Confirm to inspect the matches between metadata deployments and POD files. 
+
+In this window it will appear with green if a deployment had a correcponding file, or in red if not. 
+
+The user can choose to go back to reupload or continue with the files that did match.
+
+### 6. Process matched files
+Once the user has clicked Process files, VALKYRIE will extract porpoise detections from each POD file pair and format it to match the ETN database.
+This step can take a long time depending on the size and number of files.
+
+### 7. Evaluate ouput files
+Once the processing is done, VALKYRIE will present an overview table of the files that were processed and which that failed with a reason indicated. 
+The user can now download the output files for all the POD files that were successfully extracted. 
+
 After processing is complete, Valkyrie produces three output files:
 
 1. A deployment metadata file containing harmonized information for the processed deployments
 2. A receiver metadata file with information about the POD units used
 3. A combined detections file containing all extracted detections from all uploaded deployments
 
+### 8. Upload output files to ETN
 All output files are ready for direct ingestion into the ETN Underwater Acoustics database with no further formatting required.
 
-## Support
-For issues, feature requests, or bug reports, please open an issue in this repository.
-If you need additional help preparing metadata or understanding the ETN submission requirements, please consult the ETN documentation or contact the project maintainers.
-
-## Progress
-The tool is under development in a collaboration between Aarhus University and VLIZ. 
-
-
+## Meta
+- License: MIT
+- Please note that by using VALKYRIE and registering to ETN you agree to the following Data Policy [link].
---
+++ oldScripts/FP3read_DTO_debug.m
@@ -0,0 +1,393 @@
+function [minutes, trains]=FP3read_DTO_debug(path,filename, n)
+% [minutes, trains]=FP3read(filename, n)
+% Reads F-POD FP3-datafile
+% returns structures "minutes" with data arranged per minute and
+% "trains" with data arranged in individual trains
+% Filename without extension or .'FP3'.
+% Requires both FP1 and FP3-files to be in the current directory in order
+% to compute noise level nall
+% Variable n = '-n' includes Nall from FP1-file (if present)
+
+% Info from FP3-file is stored in two structures, minutes and trains.
+% Minutes-structure contains redordings minute by minute, also empty
+% minutes.
+% minutes.time          : beginning of minute, in Matlab format
+% minutes.temperature   : in oC
+% minutes.angle         : degrees from vertical (hydrophone up
+% minutes.no_of_trains  : trains in minute
+% minutes.no_of_clicks  : clicks in minute
+% minutes.trainHi       : Trains classified as High probability
+% minutes.trainMed      : Trains classified as Medium probability
+% minutes.trainLow      : Trains classified as Low probability
+% minutes.trainAll      : Trains classified as Doubtful
+% minutes.clickHi       : clicks classified as High probability
+% minutes.clickMed      : clicks classified as Medium probability
+% minutes.clickLow      : clicks classified as Low probability
+% minutes.clickAll      : clicks classified as Doubtful
+% minutes.train.ID              : ID number of train. not unique
+% minutes.train.spclass         : 0=NBHF, 1= dolphin, 2=noSp, 3=sonar
+% minutes.train.species         : text
+% minutes.train.qualityclass    : 0=doubt, 1=low, 2=med, 3=hi
+% minutes.train.quality         : text
+% minutes.train.rategood        : Boolean, use unknown
+% minutes.train.speciesgood     : Boolean, use unknown
+% minutes.train.no_of_clicks    : clicks in train
+% minutes.train.time            : time of individual clicks, usecs after start of minute
+% minutes.train.ici             : Inter-click-interval, usec
+
+
+% trains-structure contains the same information about individual trains as
+% the minutes-strucure, just organised by individual trains, rather than by
+% minute.
+
+% There is more information in the FP3-file, not all is extracted.
+
+% J. Tougaard, Aarhus University, May 2017
+% Modified by Mel Cosentino, Aarhus University, Nov 2023, to read FP3 files
+% Modified by Mia L. K. Nielsen and Ellen Jacobs, Aarhus University, March-September 
+% 2025 based on input from Nick Treganza
+
+% FP3-file structure (from Nick):
+% The FP3 file structure is different than the CP3 file structure. There is
+% a longer header and data is stored in 16-bit segments. The information
+% that is stored in each byte of the segment will depend on the number in
+% the first byte (see below). 
+% But similar to the CP3 data 254 denotes minute data.
+
+% Click data:
+% 0-183: Means the following 15 bytes will have click data information
+% % Byte 0: Time (MSB): time stamp: steps of 0.5 micro-s from start of current minute, big-endian,
+% % Byte 1: time
+% % Byte 2: Time (LSB)
+% % Byte 3: Ncyc: N of cycles in the click - up to 255cycles
+% % Byte 4: Peakat #ClkIPIrange: Wavenumber of loudest cycle; range of IPIs in click
+% % Byte 5: IPIpreMax: IPI  of Pk-1
+% % Byte 6: IPIatMax: IPI of  Pk  this is the loudest cycle in the click
+% % Byte 7: IPIplus1: IPI of  Pk+1
+% % Byte 8: IPIplus2: IPI of  Pk+2
+% % Byte 9: RawPkminus1: Amplitude of  P-1
+% % Byte 10: MaxPkRaw: Amplitude of  Pmax,  the loudest cycle in the click
+% % Byte 11: RawPkplus1: Amplitude of  P+1
+% % Byte 12: IPIbefore: IPI before click start
+% % Byte 13: AmpReversals #duration: N of Amplitude Reversals in click envelope; Duration of click (MSB)
+% % Byte 14: duration: Duration of click (LSB)
+% % Byte 15: HasWave #EndIPI: IPI of last cycle, compressed; flag if boat sonar found
+
+% Train details
+% 249: Indicates that the following 15 bytes are train information
+% % Byte 1: tNinIncGaps #clusterNall
+% % Byte 2: tNinIncGaps
+% % Byte 3: tMedKHZ
+% % Byte 4: tAvSPL
+% % Byte 5: tAvPkAt #tAvPRF
+% % Byte 6: tAvPRF
+% % Byte 7: PrecedingGap #tAvBWx8 #tWUTSrisk
+% % Byte 8: 
+% % Byte 9: tRateGoodScore
+% % Byte 10: tavNcyc
+% % Byte 11: cFmin
+% % Byte 12: cFmax
+% % Byte 13: EncSpN
+% % Byte 14: Qn #SpClass #Marked #IsEcho #SpGood #RateGood
+% % Byte 15: #TrnIDn
+
+% Minute data
+% 254: Indicates that the following 15 bytes are minute information
+% % Byte 1: VirtLastClkTimeInMin
+% % Byte 2: 
+% % Byte 3: AngleDeg
+% % Byte 4: 
+% % Byte 5: 
+% % Byte 6: LandmarkSeqScore
+% % Byte 7: DegC
+% % Byte 8: NrawClxInMin
+% % Byte 9: NrawClxInMin
+% % Byte 10: PriorMinOn #FollowingMinON #BatInUse #BatInUse
+% % Byte 11: battery stuff
+% % Byte 12: battery stuff
+% % Byte 13: battery stuff
+% % Byte 14: MinuteDeadband
+% % Byte 15: 
+
+% Other values may appear and then the following bytes will hold different
+% information. These are not relevant for this project, but the vlaues and
+% their meaning are outlined here:
+% 247: Text error code
+% 248: social call parameters
+% 250: wev recording info
+% 252: sonar ghosts record
+% 253: Duff record
+% 255: end of recording 
+
+% First 7008 bytes: Header 
+% Byte 257-260: Starttime in minutes from 1899-12-30 00:00, big-endian
+
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% 
+% Troubleshooting: 
+% [files, path] = uigetfile('*.*', 'MultiSelect','on') ; % select files to
+% load and test the script on
+% filename = files{2} ; % get filename of selected file
+   
+if strcmp('.FP3',filename(end-3:end)) || strcmp('.fp3',filename(end-3:end))
+    filename=filename(1:end-4);     %remove extension
+end
+%file=fopen([filename,'.FP3']);
+file = fopen([path,'\', filename, '.FP3']) ; % includes path as argument 
+FP3_data=fread(file,[16,inf],'*uint8'); % read in so table fits with the 16-bit format - first 65 rows is the header
+FP3_data = FP3_data'; % transpose data so each row is 16-byte minute info and the first column will indicate what information each byte/column will hold (see above)
+fclose(file);
+
+% Get starttime from the header
+starttime = double(FP3_data(17,1:4));
+starttime = ((starttime(1)*256 + starttime(2))*256 + starttime(3))*256 + starttime(4);
+starttimeFP3=datenum([1899 12 30 0 starttime 0]); % changed from datenum
+%datebase = 1899-12-30 00:00
+
+% save header for later
+headerBytes = FP3_data(1:64,:);
+headerFlat = reshape(headerBytes',[],1) ; % convert to 1024x1 vector
+
+FP3_data=FP3_data(65:end,:) ; % remove header information
+FP3_data(ismember(FP3_data(:,1), [247, 248, 250, 252, 253, 255]), :) = []; % delete unused markers (247, 248, 250, 252, 253 and 255)
+
+if nargin>2 && strcmp('-n',n) % set nargin>1 if the path argument is removed
+    try     %Read FP1-file, if present
+        % file=fopen([filename,'.FP1']);
+        file = fopen([path, '\', filename, '.FP1']);
+        noFP1=false;
+    catch
+        disp('FP1-file not found!');
+        noFP1=true;
+    end
+else
+    noFP1=true;
+end
+if ~noFP1
+    FP1_data=fread(file,[16,inf],'*uint8');
+    fclose(file);
+
+    FP1_data=FP1_data';
+    FP1_data=FP1_data(65:end,:); % remove header information
+    FP1_data(ismember(FP1_data(:,1), 255), :) = []; %delete end of file markers
+    
+end
+
+%% Find minute-recordings
+if ~noFP1
+    minutebreaksFP1=FP1_data(:,1)==254;    %lines with minutedata - same length as original file
+    dummy=(1:length(FP1_data));            %linenumber in raw data
+    minuteindexFP1=dummy(minutebreaksFP1);  %index to location of minutebreaks in data
+end
+
+minutebreaks=FP3_data(:,1)==254;       % Find all minutebreaks (indicated by 254 in column 1) - logical - same length as original file
+dummy=(1:length(FP3_data));            % linenumber in raw data
+minuteindex=dummy(minutebreaks);        % index to location of minutebreaks in data
+
+% OBS: if minuteindex and minuteindexFP1 are same length that's a good
+% sign as they should have the data for same period!
+
+%% Extract version info from header
+FPGAcodeVersion = headerFlat(40)*256 + headerFlat(41);  % bytes 39–40 (MATLAB is 1-based)
+HasExtendedAmps = FPGAcodeVersion > 801; % determine if version is >801, which will be important for filtering options
+
+%% Read clicks minute by minute
+qualitylist={'Doubtful','Low','Medium','High'};
+specieslist={'NBHF','Dolphin','Unclass.','Sonar','possibleSonar'};
+minutes=struct;
+trains=struct;
+trainno=1;
+for currentminute=1:sum(minutebreaks)-1
+    minutes(currentminute).time=starttimeFP3+(currentminute-1)/1440; % convert time to the right format
+    minutes(currentminute).temperature=FP3_data(minuteindex(currentminute),8); % get temperature in current minute from column 8
+    minutes(currentminute).angle=round(acosd(1-double(FP3_data(minuteindex(currentminute),4))/128)); % get angle in current minute from column 4
+    if minuteindex(currentminute+1)>minuteindex(currentminute)+1   %minute not empty 
+        clicksinminute=FP3_data(minuteindex(currentminute)+1:minuteindex(currentminute+1)-1,:); %all clickinfo in current minute (between the minute breaks)
+        clickdata = clicksinminute(clicksinminute(:,1)<=183,:); % get click data record for minute - needed for freq filtering and train duration
+        clicksinminute = clicksinminute(clicksinminute(:,1)==249,:); % get train details data for a minute
+
+        % Train details:
+        trainID=clicksinminute(:,16); % get all train IDs in the minute - are in column 16 if column 1 == 249
+        trainIDlist=unique(trainID); % get unique train IDs in minute
+        minutes(currentminute).no_of_trains=length(trainIDlist); % get number of trains in minute
+        minutes(currentminute).no_of_clicks=length(trainID);     % get number of clicks in minute
+        for n=1:length(trainIDlist) % for the unique trains identified
+            minutes(currentminute).train(n).ID=trainIDlist(n); % get the nth train in minute
+            trains(trainno).ID=trainIDlist(n); % the nth train in train form
+            dummy=clicksinminute(trainID==trainIDlist(n),15); % the field with info on species class, quality class, species good and rate good - in column 15 if column 1 == 249
+            % Species class % % From Nicks code: tSpClass(Fs[FN].FBuf[Fs[FN].BufPosn + 14] and 12 shr 2); // (spHP, spDOL, spUNX, spSON, spPossSON);
+            minutes(currentminute).train(n).spclass=bitshift(bitand(dummy(1),12),-2); % 
+            trains(trainno).spclass=bitshift(bitand(dummy(1),12),-2);
+            minutes(currentminute).train(n).species=specieslist(trains(trainno).spclass+1);
+            trains(trainno).species=specieslist(trains(trainno).spclass+1);
+            % quality class % % From Nicks code: Fs[FN].FBuf[Fs[FN].BufPosn + 14] and 3;        // train Qvalue 3 is Hi
+            minutes(currentminute).train(n).qualityclass=bitand(dummy(1),3);
+            trains(trainno).qualityclass=bitand(dummy(1),3);
+            minutes(currentminute).train(n).quality=qualitylist(trains(trainno).qualityclass+1);
+            trains(trainno).quality=qualitylist(trains(trainno).qualityclass+1);
+            % Rate good? (boolean) % % From Nicks code: Fs[FN].FBuf[Fs[FN].BufPosn + 14] and 128 = 128 then Fs[FN].NRClk.RateGood:= true;
+            minutes(currentminute).train(n).rategood=(bitand(dummy(1),128) == 128);
+            trains(trainno).rategood=(bitand(dummy(1),128) == 128);
+            % species good (boolean) % % From Nicks code: Fs[FN].FBuf[Fs[FN].BufPosn + 14] and 64 = 64   then Fs[FN].NRClk.SpGood:=   true;
+            minutes(currentminute).train(n).speciesgood=(bitand(dummy(1),64) == 64);
+            trains(trainno).speciesgood=(bitand(dummy(1),64) == 64);
+            % Number of clicks in train
+            minutes(currentminute).train(n).no_of_clicks=sum(trainID==trainIDlist(n));
+            trains(trainno).no_of_clicks=sum(trainID==trainIDlist(n));
+            % Date and time of current minute
+            trains(trainno).minute=starttimeFP3+currentminute/1440;
+            % Time of each click in the train
+
+            % OLD
+            %minutes(currentminute).train(n).time = 2*(((clickdata(trainID==trainIDlist(n),1)*256 ... % multiply by 2 bc it is 0.5 ms chunks instead of 0.2 now
+            %     +clickdata(trainID==trainIDlist(n),2))*256) + clickdata(trainID==trainIDlist(n),3));
+            %trains(trainno).time = 2*(((clickdata(trainID==trainIDlist(n),1)*256 ... 
+            %     +clickdata(trainID==trainIDlist(n),2))*256) + clickdata(trainID==trainIDlist(n),3));
+
+            % NEW
+            minutes(currentminute).train(n).time = 2*(((double(clickdata(trainID==trainIDlist(n),1))*256 ... % REALLY NOT SURE ABOUT THIS MULTIPLICATION FACTOR
+                + double(clickdata(trainID==trainIDlist(n),2)))*256) + double(clickdata(trainID==trainIDlist(n),3)));
+            trains(trainno).time = 2*(((double(clickdata(trainID==trainIDlist(n),1))*256 ...
+                + double(clickdata(trainID==trainIDlist(n),2)))*256) + double(clickdata(trainID==trainIDlist(n),3)));
+
+            minutes(currentminute).train(n).ici=diff([minutes(currentminute).train(n).time]);
+            trains(trainno).ici=diff([minutes(currentminute).train(n).time]);
+            trainno=trainno+1;
+        end
+        
+        minutes(currentminute).trainHi=sum([minutes(currentminute).train.spclass]==0'&...
+            [minutes(currentminute).train.qualityclass]==3);
+        minutes(currentminute).trainMed=sum([minutes(currentminute).train.spclass]==0'&...
+            [minutes(currentminute).train.qualityclass]==2);
+        minutes(currentminute).trainLow=sum([minutes(currentminute).train.spclass]==0'&...
+            [minutes(currentminute).train.qualityclass]==1);
+        minutes(currentminute).trainAll=sum([minutes(currentminute).train.spclass]==0'&...
+            [minutes(currentminute).train.qualityclass]==0);
+        
+        minutes(currentminute).clickHi=sum([minutes(currentminute).train(...
+            [minutes(currentminute).train.spclass]'==0&...
+            [minutes(currentminute).train.qualityclass]'==3).no_of_clicks]);
+        minutes(currentminute).clickMed=sum([minutes(currentminute).train(...
+            [minutes(currentminute).train.spclass]'==0&...
+            [minutes(currentminute).train.qualityclass]'==2).no_of_clicks]);
+        minutes(currentminute).clickLow=sum([minutes(currentminute).train(...
+            [minutes(currentminute).train.spclass]'==0&...
+            [minutes(currentminute).train.qualityclass]'==1).no_of_clicks]);
+        minutes(currentminute).clickAll=sum([minutes(currentminute).train(...
+            [minutes(currentminute).train.spclass]'==0&...
+            [minutes(currentminute).train.qualityclass]'==0).no_of_clicks]);
+    else    % No clicks in minute, set all variables to zero
+        minutes(currentminute).no_of_trains=0;
+        minutes(currentminute).no_of_clicks=0;
+        minutes(currentminute).trainHi=0;
+        minutes(currentminute).trainMed=0;
+        minutes(currentminute).trainLow=0;
+        minutes(currentminute).trainAll=0;
+        minutes(currentminute).clickHi=0;
+        minutes(currentminute).clickMed=0;
+        minutes(currentminute).clickLow=0;
+        minutes(currentminute).clickAll=0;
+    end
+    if ~noFP1   %Get nall from FP1-file, if present
+        if minuteindexFP1(currentminute+1)>minuteindexFP1(currentminute)+1   %FP1 minute not empty
+            allRecords = FP1_data(minuteindexFP1(currentminute)+1 : ...
+                      minuteindexFP1(currentminute+1)-1, :);
+
+            %Extract only click records (0–183)
+            clicksinminute = allRecords(allRecords(:,1) <= 183, :);
+            
+            %Filter sonar clicks
+            %clicksinminute(clicksinminute(:,16) < 2, :) = [];
+            
+            
+            % --------------------------------------------
+            % Filter clicks based on KERNO-F settings (for nall to match
+            % output of FPOD.exe)
+            % --------------------------------------------
+            % Based on both Nick's code and Jamie's Java script filtering
+            % based on frequency is always based on the frequncy derived
+            % from IPI unless the user has checked a box to use the
+            % duration-derived frequency. This script has implemented both
+            % so we can include the duration-derived one if we find out it
+            % is something that has been used (most will have used the
+            % default).
+            % % Filter based on clicks that are evaluated by the KERNO-F
+            % % version 1 classifier - amplitude > 10 and between 22 and 221
+
+            % % Extract values
+            rawPk = double(clicksinminute(:,11));
+            rawPk(rawPk < 2) = 2;
+            IPIatMax = double(clicksinminute(:, 7)) + 1 ; % IPI is byte 6 + 1
+
+            % Use header-derived HasExtendedAmps
+            realAmp = RawToRealPk(rawPk, IPIatMax, HasExtendedAmps);
+
+            % Now apply amplitude thresholding (e.g., for KERNO-F classifier)
+            clicksinminute(realAmp <= 11, :) = [];
+
+            % % Update/extract values after filtering
+            Ncyc = double(clicksinminute(:,3));% Number if cycles
+            IPIatMax = double(clicksinminute(:, 7)) + 1 ; % IPI is byte 6 + 1
+            duration = bitor(bitshift(bitand(double(clicksinminute(:,14)), 240), 4), double(clicksinminute(:,15)));
+
+            % % Get Frequency from IPI (always used)
+            ClkKHZpk = round(4000 ./ IPIatMax);  % same in Delphi + Java
+
+
+            % % Frequency from duration (conditionally used)
+            ClkKHZdur = ClkKHZpk; % default fallback
+
+            % Duration-based frequency rules from Delphi/Nick's code
+            midrange = (Ncyc >= 5 & Ncyc < 16) & (duration >= 100);
+            longclicks = (Ncyc >= 16) & (duration >= 100);
+
+            ClkKHZdur(midrange)  = round((4000 .* (Ncyc(midrange) - 3)) ./ duration(midrange));
+            ClkKHZdur(longclicks) = round(60000 ./ duration(longclicks));
+
+            % Delphi code: If discrepancy >30 kHz, revert to IPI estimate
+            diffTooBig = abs(ClkKHZdur - ClkKHZpk) > 30;
+            ClkKHZdur(diffTooBig) = ClkKHZpk(diffTooBig);
+
+            % Determine which KHZ value is in use
+            % Note: In FPOD.exe, this is user-controlled. Default is to use ClkKHZpk
+            % If we want we can implement using duration-derived frequency:
+            useDurationFreq = false; % set true to match "ShowKHZfromDuration.Checked" from Nick's code
+
+            if useDurationFreq
+                ClkKHZinUse = ClkKHZdur;
+            else
+                ClkKHZinUse = ClkKHZpk;
+            end
+
+            validFreq = ClkKHZinUse >= 21 & ClkKHZinUse <= 221;
+            clicksinminute = clicksinminute(validFreq, :);
+
+            minutes(currentminute).nall=size(clicksinminute,1); % nall: number of valid clicks in minute (after filtering by amplitude and frequency)
+
+        else
+            minutes(currentminute).nall=0;
+        end
+        % Test of whether Nall can be extracted correctly from FP3 files
+
+        byte9  = double(FP3_data(minuteindex(currentminute), 9));
+        byte10 = double(FP3_data(minuteindex(currentminute),10));
+
+        minutes(currentminute).nall = byte9 * 256 + byte10;
+
+
+    end
+    % PLACEHOLDER: add in minON to match the CP3read format
+    
+    if ~noFP1 % will have a different number of cols if noFP1
+        if minutes(currentminute).nall==0 %no clicks? prob off
+            minutes(currentminute).minON = 0 ;
+        else %clicks? prob on
+            minutes(currentminute).minON = 1 ;
+        end
+    end
+end
+
+%add in the file name where detections came from
+[minutes.filename] = deal(filename) ;
+[trains.filename] = deal(filename) ;
